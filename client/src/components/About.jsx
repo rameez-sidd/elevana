480659e8b2bde46e96c24f8562bebc6e46e5bd05
rameez-sidd <rameezkainbox@gmail.com>
2025-08-07 16:59:27 +0530

    Testimonials responsive

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -5,15 +5,15 @@ import { useSelector } from 'react-redux'
 const About = () => {
   const { user } = useSelector((state) => state.auth)
   return (
-    <div className={`${user && user?.role === 'admin' ? 'bg-light-green' : 'bg-background-green'} pb-14 md:px-4 md:pt-14 xl:px-0 min-h-screen flex items-center`} id='about'>
+    <div className={`${user && user?.role === 'admin' ? 'bg-light-green' : 'bg-background-green'} pb-14 md:py-23 md:px-4 md:pt-14 xl:px-0 min-h-screen flex items-center`} id='about'>
       <div className='mx-auto max-w-7xl w-full'>
-        <div className='flex flex-col md:flex-row gap-10 md:gap-0 xl:gap-18 '>
+        <div className='flex flex-col md:flex-row gap-8 md:gap-0 xl:gap-18 '>
           <div className='flex items-center p-2 xs:p-5 md:p-0'>
             <img src={aboutImage} alt="" width={1350} className='md:w-[2500px] lg:w-[1350px]' />
           </div>
           <div className='flex flex-col px-2 xs:px-2.5 sm:px-3.5 md:pl-6 md:pr-2 xl:pr-6 2xl:px-0 gap-5 sm:gap-7 md:gap-6 lg:gap-10 items-center justify-center'>
             <div className='flex flex-col gap-0 sm:gap-1.5 md:gap-2'>
-              <h3 className='text-center text-[23px] sm:text-2xl md:text-3xl lg:text-4xl font-[700]'>Inside <span className='text-grass-green'>Elevana</span></h3>
+              <h3 className='text-center text-[23px] sm:text-2xl md:text-3xl lg:text-4xl font-[700]'>A Look Inside <span className='text-grass-green'>Elevana</span></h3>
               <p className='text-center text-[13px] sm:text-[15px] md:text-[17px] lg:text-lg g text-gray-600'>The purpose and people behind the platform</p>
             </div>
             <p className='hidden md:block lg:hidden text-justify text-sm sm:text-[15px] lg:text-base leading-6 sm:leading-6.5'>
--- client/src/components/StudentReviews.jsx
+++ client/src/components/StudentReviews.jsx
@@ -36,27 +36,27 @@ export const reviews = [
 
 const StudentReviews = () => {
     return (
-        <div className='min-h-screen flex items-center bg-dark-green'>
-            <div className='mx-auto max-w-7xl flex flex-col gap-14 w-full'>
-                <div className='flex flex-col gap-2'>
-                    <h3 className='text-center text-4xl font-[700] text-white'>Trusted by <span className='text-grass-green'>Learners</span></h3>
-                    <p className='text-center text-lg text-gray-300'>Real experiences from real students who took the leap</p>
+        <div className='min-h-screen flex items-center bg-dark-green py-12 pb-14 sm:py-18 sm:pb-20 md:py-23 md:pb-24 px-1.5 sm:px-3 md:px-5'>
+            <div className='mx-auto max-w-7xl flex flex-col gap-6 sm:gap-9 md:gap-14 w-full'>
+                <div className='flex flex-col gap-0 sm:gap-1.5 md:gap-2'>
+                    <h3 className='text-center text-[23px] sm:text-2xl md:text-3xl lg:text-4xl font-[700] text-white'>Trusted by <span className='text-grass-green'>Learners</span></h3>
+                    <p className='text-center text-[13px] sm:text-[15px] md:text-[17px] lg:text-lg text-gray-300'>Genuine stories from real learners</p>
                 </div>
-                <div className='grid grid-cols-2  gap-8'>
+                <div className='grid grid-cols-2 gap-2.5 sm:gap-3 md:gap-4 lg:gap-8'>
                     {
                         reviews.map((review, index) => (
-                            <div key={index} className=' bg-light-green flex flex-col gap-6 p-4 px-6 rounded-md relative'>
+                            <div key={index} className='col-span-2 sm:col-span-1 bg-light-green flex flex-col justify-between gap-4 md:gap-6 p-2.5 md:p-3 md:px-4 lg:py-4 lg:px-6 rounded-md relative'>
                                 <div className='flex flex-col gap-1'>
-                                    <BiSolidQuoteLeft className='text-dark-green' size={30} />
-                                    <p className='text-sm'>{review.comment}</p>
+                                    <span className='text-2xl md:text-[30px]'><BiSolidQuoteLeft className='text-dark-green' /></span>
+                                    <p className='text-xs md:text-sm'>{review.comment}</p>
                                 </div>
-                                <div className='flex justify-between items-end'>
+                                <div className='flex justify-between gap-3 items-end'>
 
-                                    <div className='flex items-center gap-5'>
-                                        <div><img src={review.avatar} width={50} className='rounded-full outline-3 outline-white' /></div>
+                                    <div className='grid grid-cols-[auto_1fr] items-center gap-3 lg:gap-5'>
+                                        <div className=''><img src={review.avatar} className='w-[35px] md:w-[42px] lg:w-[50px] rounded-full outline-3 outline-white' /></div>
                                         <div className='flex flex-col'>
-                                            <h5 className='font-[600] text-md'>{review.name}</h5>
-                                            <p className='text-xs text-gray-600'>{review.profession}</p>
+                                            <h5 className='font-[600] text-sm md:text-base'>{review.name}</h5>
+                                            <p className='text-[11px] md:text-xs text-gray-600 line-clamp-1 overflow-x-hidden max-w-[200px] text-ellipsis'>{review.profession}</p>
                                         </div>
                                     </div>
                                     <Rating value={Math.random() * (5 - 3.5) + 3.5} precision={0.5} readOnly size='small' />
